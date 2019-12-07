07e1488389b251007fe92535826a25f809721934
Nazarkon <nazarmontsibovych@gmail.com>
2019-12-07 17:19:36 +0200

    Simple work with variables

---
+++ Ruby Simple/test.rb
@@ -0,0 +1,28 @@
+puts'Hello Wolrd!'
+var = 10
+puts var
+
+var = 9
+
+puts "#{var}"
+
+Constant = 9
+
+puts Constant
+
+Constant = 10
+
+puts Constant
+
+puts "#{2.class}"
+puts "#{3.14.class}"
+puts "#{1243e-2.class}"
+puts "#{'hello'.class}"
+
+pp "Input number"
+
+a = gets.to_f
+b = a.ceil
+puts b
+c = a.round
+puts c
